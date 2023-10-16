39121cc6910fb436da37db1c2a459cf9c31cd58f
krillan <pushkinidze49@gmail.com>
2023-10-16 10:28:14 +0300

    updates for js scripts

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,2 +1,8 @@
 // Entry point for the build script in your package.json
-import * as bootstrap from "bootstrap"
+// import * as bootstrap from "bootstrap"
+
+import Rails from "@rails/ujs"
+import Turbolinks from "turbolinks"
+
+Rails.start()
+Turbolinks.start()
