ad2789bbc1a17ff90e7c4cae454a021ed0788fbf
krillan <pushkinidze49@gmail.com>
2023-11-05 13:08:36 +0300

    full_title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,24 @@
 module ApplicationHelper
+  # def nav_tab(title, url, options = {})
+  #   current_page = options.delete :current_page
+
+  #   css_class = current_page == title ? 'text-secondary' : 'text-white'
+
+  #   options[:class] = options[:class] ? options[:class] + ' ' + css_class : css_class
+    
+  #   link_to title, url, options
+  # end
+
+  # def currently_at(current_page = '')
+  #   render partial: 'shared/menu', locals: {current_page: current_page}
+  # end
+
+  def full_title(page_title = "")
+    base_title = "AskIt"
+    if page_title.present?
+      "#{page_title} | #{base_title}"
+    else
+      base_title
+    end
+  end
 end
