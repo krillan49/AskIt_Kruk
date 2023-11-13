ae3111b8c9de3e0f475164af188447891c9d360e
krillan <pushkinidze49@gmail.com>
2023-11-13 15:13:27 +0300

    migration from kaminary to pagy

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,7 +2,7 @@ class QuestionsController < ApplicationController
   before_action :set_question!, only: %i[show destroy edit update]
 
   def index
-    @questions = Question.order(created_at: :desc).page params[:page]
+    @pagy, @questions = pagy Question.order(created_at: :desc)
   end
 
   def new 
@@ -21,7 +21,7 @@ class QuestionsController < ApplicationController
 
   def show
     @answer = @question.answers.build
-    @answers = @question.answers.order(created_at: :desc).page(params[:page])
+    @pagy, @answers = pagy @question.answers.order(created_at: :desc)
   end
 
   def edit
