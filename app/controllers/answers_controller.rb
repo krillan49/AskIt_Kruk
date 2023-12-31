b0e81330539c39fdfe4aeb51696ebb35a7f42d9c
krillan <pushkinidze49@gmail.com>
2023-12-31 16:05:01 +0300

    i18n update for controllers

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -6,13 +6,22 @@ class AnswersController < ApplicationController
   before_action :set_question!
   before_action :set_answer!, except: :create
 
+  def update
+    if @answer.update answer_update_params
+      flash[:success] = t '.success'
+      redirect_to question_path(@question, anchor: dom_id(@answer))
+    else
+      render :edit
+    end
+  end
+
   def edit; end
 
   def create
-    @answer = @question.answers.build answer_params
+    @answer = @question.answers.build answer_create_params
 
     if @answer.save
-      flash[:success] = 'Answer created!'
+      flash[:success] = t '.success'
       redirect_to question_path(@question)
     else
       @question = @question.decorate
@@ -22,24 +31,19 @@ class AnswersController < ApplicationController
     end
   end
 
-  def update
-    if @answer.update answer_params
-      flash[:success] = 'Answer updated!'
-      redirect_to question_path(@question, anchor: dom_id(@answer))
-    else
-      render :edit
-    end
-  end
-
   def destroy
     @answer.destroy
-    flash[:success] = 'Answer deleted!'
+    flash[:success] = t '.success'
     redirect_to question_path(@question)
   end
 
   private
 
-  def answer_params
+  def answer_create_params
+    params.require(:answer).permit(:body).merge(user: current_user)
+  end
+
+  def answer_update_params
     params.require(:answer).permit(:body)
   end
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,32 +5,31 @@ class UsersController < ApplicationController
   before_action :require_authentication, only: %i[edit update]
   before_action :set_user!, only: %i[edit update]
 
-  def new
-    @user = User.new
-  end
-
   def edit; end
-
-  def create
-    @user = User.new user_params
-    if @user.save
-      sign_in @user
-      flash[:success] = "Welcome to the app, #{current_user.name_or_email}!"
-      redirect_to root_path
-    else
-      render :new
-    end
-  end
 
   def update
     if @user.update user_params
-      flash[:success] = 'Your profile was successfully updated!'
+      flash[:success] = t '.success'
       redirect_to edit_user_path(@user)
     else
       render :edit
     end
   end
 
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new user_params
+    if @user.save
+      sign_in @user
+      flash[:success] = t('.success', name: current_user.name_or_email)
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
 
   private
 
