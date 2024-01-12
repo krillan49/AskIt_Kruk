9e3c835ec56e5ad39a8be472f6814c28b84598fa
krillan <pushkinidze49@gmail.com>
2024-01-12 16:44:56 +0300

    link tag to questions functoins update

--- app/models/question.rb
+++ app/models/question.rb
@@ -11,20 +11,14 @@ class Question < ApplicationRecord
   validates :title, presence: true, length: { minimum: 2 }
   validates :body, presence: true, length: { minimum: 2 }
 
-  # scope :all_by_tags, ->(tags) do
-  #   questions = includes(:user)
-  #   if tags
-  #     questions = questions.joins(:tags).where(tags: tags).preload(:tags)
-  #   else
-  #     questions = questions.includes(:question_tags, :tags)
-  #   end
-     
-  #   questions.order(created_at: :desc)
-  # end
-
-  scope :all_by_tags, ->(tag_ids) do
-      questions = includes(:user, :question_tags, :tags)
-      questions = questions.joins(:tags).where(tags: tag_ids) if tag_ids
-      questions.order(created_at: :desc)
+  scope :all_by_tags, ->(tags) do
+    questions = includes(:user)
+    if tags
+      questions = questions.joins(:tags).where(tags: tags).preload(:tags)
+    else
+      questions = questions.includes(:question_tags, :tags)
     end
+     
+    questions.order(created_at: :desc)
+  end
 end
