82c7859f371e4df9c6fc0b453e5ed9147f2afca5
krillan <pushkinidze49@gmail.com>
2024-01-15 16:49:59 +0300

    tomselect add for selector

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -7,6 +7,7 @@ import Turbolinks from "turbolinks"
 import '@popperjs/core'
 import 'bootstrap/js/dist/dropdown'
 import 'bootstrap/js/dist/collapse'
+import './scripts/select'
 
 Rails.start()
 Turbolinks.start()
