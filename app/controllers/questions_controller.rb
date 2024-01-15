175534c4adc20d2a98c4638f2aeddc96418438a0
krillan <pushkinidze49@gmail.com>
2024-01-15 18:25:55 +0300

    add ajax to all form with tags

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,7 +3,6 @@
 class QuestionsController < ApplicationController
   include QuestionsAnswers
   before_action :set_question!, only: %i[show destroy edit update]
-  before_action :fetch_tags, only: %i[new edit]
 
   def show
     load_question_answers
@@ -54,9 +53,5 @@ class QuestionsController < ApplicationController
 
   def set_question!
     @question = Question.find params[:id]
-  end
-
-  def fetch_tags
-    @tags = Tag.all
   end
 end
