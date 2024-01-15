017e7d6ec4202a70eb79c72d0eac7be5376cadb2
krillan <pushkinidze49@gmail.com>
2024-01-15 15:19:21 +0300

    add tag unique index & validation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_01_12_101944) do
+ActiveRecord::Schema[7.0].define(version: 2024_01_15_120121) do
   create_table "answers", force: :cascade do |t|
     t.text "body", null: false
     t.integer "question_id", null: false
@@ -55,6 +55,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_01_12_101944) do
     t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["title"], name: "index_tags_on_title", unique: true
   end
 
   create_table "users", force: :cascade do |t|
