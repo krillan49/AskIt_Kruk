ce157e184ddf9ec709a05b6682bf6f8434dc9589
krillan <pushkinidze49@gmail.com>
2024-01-17 17:12:23 +0300

    add enum-role to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  enum role: { basic: 0, moderator: 1, admin: 2 }, _suffix: :role
+  
   attr_accessor :old_password, :remember_token
 
   has_secure_password validations: false
