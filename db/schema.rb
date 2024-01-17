5f7d0af614df83e84c987152d80a325ccc74bf28
krillan <pushkinidze49@gmail.com>
2024-01-17 16:49:23 +0300

    add role field in users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_01_15_120121) do
+ActiveRecord::Schema[7.0].define(version: 2024_01_17_133634) do
   create_table "answers", force: :cascade do |t|
     t.text "body", null: false
     t.integer "question_id", null: false
@@ -66,7 +66,9 @@ ActiveRecord::Schema[7.0].define(version: 2024_01_15_120121) do
     t.datetime "updated_at", null: false
     t.string "remember_token_digest"
     t.string "gravatar_hash"
+    t.integer "role", default: 0, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["role"], name: "index_users_on_role"
   end
 
   add_foreign_key "answers", "questions"
