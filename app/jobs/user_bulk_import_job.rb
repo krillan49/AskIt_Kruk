78a57ee5281660f56ba74bbf4399e8d1750c7545
krillan <pushkinidze49@gmail.com>
2024-02-07 18:46:48 +0300

    perform & admin mailers for zip-job

--- app/jobs/user_bulk_import_job.rb
+++ app/jobs/user_bulk_import_job.rb
@@ -4,10 +4,10 @@ class UserBulkImportJob < ApplicationJob
   queue_as :default
 
   def perform(archive_key, initiator)
-  #   UserBulkImportService.call archive_key
-  # rescue StandardError => e
-  #   Admin::UserMailer.with(user: initiator, error: e).bulk_import_fail.deliver_now
-  # else
-  #   Admin::UserMailer.with(user: initiator).bulk_import_done.deliver_now
+    UserBulkImportService.call archive_key
+  rescue StandardError => e
+    Admin::UserMailer.with(user: initiator, error: e).bulk_import_fail.deliver_now
+  else
+    Admin::UserMailer.with(user: initiator).bulk_import_done.deliver_now
   end
 end
