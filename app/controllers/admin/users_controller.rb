757e1c023f1d90d2978082b972700b190c55ea25
krillan <pushkinidze49@gmail.com>
2024-02-09 17:35:45 +0300

    export service & index change

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -13,7 +13,11 @@ module Admin
           @pagy, @users = pagy User.order(created_at: :desc)
         end
 
-        format.zip { respond_with_zipped_users }
+        format.zip do
+          UserBulkExportJob.perform_later current_user
+          flash[:success] = t '.success'
+          redirect_to admin_users_path
+        end
       end
     end
 
@@ -51,20 +55,6 @@ module Admin
                                                       filename: params[:archive].original_filename
       file.close
       result.key
-    end
-
-    def respond_with_zipped_users
-      compressed_filestream = Zip::OutputStream.write_buffer do |zos|
-        User.order(created_at: :desc).each do |user|
-          zos.put_next_entry "user_#{user.id}.xlsx"
-          zos.print render_to_string(
-            layout: false, handlers: [:axlsx], formats: [:xlsx], template: 'admin/users/user', locals: { user: user }
-          )
-        end
-      end
-
-      compressed_filestream.rewind
-      send_data compressed_filestream.read, filename: 'users.zip'
     end
 
     def set_user!
