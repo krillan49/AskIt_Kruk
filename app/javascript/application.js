de3ef452a46b271e29f38b395757097d76c0c74b
krillan <pushkinidze49@gmail.com>
2024-02-16 17:41:08 +0300

    esbuild install & application.js update

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -4,7 +4,6 @@
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 
-import '@popperjs/core'
 import 'bootstrap/js/dist/dropdown'
 import 'bootstrap/js/dist/collapse'
 import './scripts/select'
