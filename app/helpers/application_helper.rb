429fe07e6889c9d47a69728329465c7417fc71c7
krillan <pushkinidze49@gmail.com>
2024-02-26 18:18:59 +0300

    Flash turbo frame create question

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,6 +3,10 @@
 module ApplicationHelper
   include Pagy::Frontend
 
+  def prepend_flash
+    turbo_stream.prepend 'flash', partial: 'shared/flash'
+  end
+
   def pagination(obj)
     # rubocop:disable Rails/OutputSafety
     raw(pagy_bootstrap_nav(obj)) if obj.pages > 1
@@ -11,13 +15,20 @@ module ApplicationHelper
 
   def nav_tab(title, url, options = {})
     current_page = options.delete :current_page
+
     css_class = current_page == title ? 'text-secondary' : 'text-white'
-    options[:class] = options[:class] ? "#{options[:class]} #{css_class}" : css_class
+
+    options[:class] = if options[:class]
+                        "#{options[:class]} #{css_class}"
+                      else
+                        css_class
+                      end
+
     link_to title, url, options
   end
 
   def currently_at(current_page = '')
-    render partial: 'shared/menu', locals: { current_page: }
+    render partial: 'shared/menu', locals: { current_page: current_page }
   end
 
   def full_title(page_title = '')
